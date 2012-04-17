1a95c0432398b3dd4ec9472d1427122ae5d39a9a
Gregory Haynes <greg@greghaynes.net>
2012-04-17 14:37:33 -0700

    Use channel #rtb-test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from bot import IrcBot
 
 if __name__=='__main__':
 	bot = IrcBot('irc.cat.pdx.edu', 6667, 'RTB', 'RTB')
-	bot.join('#cscapstone-b')
+	bot.join('#rtb-test')
 	bot.connect()
 	evloop.EventDispatcher().loop_forever()
 
