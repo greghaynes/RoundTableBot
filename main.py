53b3c8d837396f205049cfb13d530d735733db16
Gregory Haynes <greg@greghaynes.net>
2012-08-16 18:47:05 -0700

    Added plugin system

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import evloop
 from bot import RoundTableBot
+from plugin_loader import PluginLoader
 
 if __name__=='__main__':
+	plugin_loader = PluginLoader('plugins')
 	bot = RoundTableBot('irc.cat.pdx.edu', 6667, 'RTB', 'RTB')
 	bot.join('#rtb-test')
 	bot.connect()
+	plugin_loader.load_all(bot)
 	evloop.EventDispatcher().loop_forever()
 
---
+++ plugins/twitter_updates.py
@@ -0,0 +1,6 @@
+import plugin
+
+class TwitterUpdatesPlugin(plugin.Plugin):
+	def __init__(self, bot):
+		self.bot = bot
+		print 'hello'
